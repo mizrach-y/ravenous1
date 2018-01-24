7d2f4bdda907cfbccfda59e1c3505b125a281cdb
Misrak <misrakina@gmail.com>
2018-01-23 23:30:25 -0500

    wey taab

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component{
+  render(){
+    return(
+      <div className="BusinessList">
+  <!-- Add Business Component here -->
+  <!-- Add Business Component here -->
+  <!-- Add Business Component here -->
+  <!-- Add Business Component here -->
+  <!-- Add Business Component here -->
+  <!-- Add Business Component here -->
+</div>
+);
+  }
+}
+export default BusinessList;
